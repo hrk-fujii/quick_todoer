b8bdf96c29fdd3010a3997b94295a4ce07f26073
Hiroki Fujii <hfujii@hisystron.com>
2022-05-03 22:48:16 +0900

    ログイン状態に応じた設定画面の変更を一部実装

--- lib/setting/user_setting_item.dart
+++ lib/setting/user_setting_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 import 'setting.dart';
 import 'auth.dart';
+import '../type/firestore.dart';
+import '../provider/auth_state_provider.dart';
 
 
 class UserSettingItem extends StatelessWidget {
@@ -12,13 +16,24 @@ class UserSettingItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return SettingItem(
       title: "ユーザー設定",
-      widget: RaisedButton(
-        child: Text("ログイン"),
-        onPressed: () async {
-          await Navigator.push(context, MaterialPageRoute(
-            builder: (context) => Signin()
-          ));
-        },
+      widget: (context.select((AuthStateProvider authState) => authState.connectionState) == ConnectionState.waiting) ?
+        Center(
+          child: CircularProgressIndicator(),
+        )
+      : ((context.select((AuthStateProvider authState) => authState.userData) != null) ?
+        RaisedButton(
+          child: Text("設定"),
+          onPressed: (){}
+        )
+      : // else
+        RaisedButton(
+          child: Text("ログイン"),
+          onPressed: () async {
+            await Navigator.push(context, MaterialPageRoute(
+              builder: (context) => Signin()
+            ));
+          },
+        )
       ),
       description: "Quick Todoer を利用するには、ログインしてください。",
     )    ;
