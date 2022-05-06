231f229eb0a5e78abdf340fdf0090e18b0a225b8
Hiroki Fujii <hfujii@hisystron.com>
2022-05-06 23:47:45 +0900

    タスクリストのレイアウトを調整

--- lib/task/task_item.dart
+++ lib/task/task_item.dart
@@ -10,7 +10,7 @@ class TaskItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(5),
+      padding: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
       child: Container(
         padding: EdgeInsets.all(10),
         child: Row(
@@ -24,28 +24,36 @@ class TaskItem extends StatelessWidget {
               ),
             ),
             SizedBox(width: 10),
-            Column(
-              children: [
-                Text(
-                  "サンプルタスク",
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "サンプルタスクのサンプル説明。",
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 5),
-                Text("2022/02/22 22:22 まで。現在未着手"),
-              ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "サンプルタスク",
+                    style: TextStyle(fontSize: 15),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "サンプルタスクのサンプル説明。サンプルタスクのサンプル説明。",
+                    textAlign: TextAlign.left,
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "2022/02/22 22:22 まで。未着手",
+                    textAlign: TextAlign.right,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
         decoration: BoxDecoration(
           border: Border.all(color: Theme.of(context).colorScheme.onBackground),
-          borderRadius: BorderRadius.circular(5),
+          borderRadius: BorderRadius.circular(10),
         ),
       ),
     );
