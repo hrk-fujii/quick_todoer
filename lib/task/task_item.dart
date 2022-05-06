1e3b79f306db495e430abd36c445d487495ef900
Hiroki Fujii <hfujii@hisystron.com>
2022-05-07 00:07:23 +0900

    2アイコンと詳細情報を分離

--- lib/task/task_item.dart
+++ lib/task/task_item.dart
@@ -14,39 +14,44 @@ class TaskItem extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.all(10),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             IconButton(
               onPressed: (){},
               icon: Icon(
                 Icons.remove,
-                semanticLabel: "未着手",
+                semanticLabel: "未着手のサンプルタスク",
                 size: 50
               ),
             ),
             SizedBox(width: 10),
             Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "サンプルタスク",
-                    style: TextStyle(fontSize: 15),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "サンプルタスクのサンプル説明。サンプルタスクのサンプル説明。",
-                    textAlign: TextAlign.left,
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "2022/02/22 22:22 まで。未着手",
-                    textAlign: TextAlign.right,
-                  ),
-                ],
+              child: InkWell(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "サンプルタスク",
+                      style: TextStyle(fontSize: 15),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "サンプルタスクのサンプル説明。サンプルタスクのサンプル説明。",
+                      textAlign: TextAlign.left,
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "2022/02/22 22:22 まで。未着手",
+                      textAlign: TextAlign.right,
+                    ),
+                  ],
+                ),
+                onTap: () {},
               ),
             ),
           ],
